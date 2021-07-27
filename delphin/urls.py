f927450f6cbfc2eee31a2fd8734ff6ea84b859ca
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-27 19:13:26 +0000

    Add navbar.html and css

--- delphin/urls.py
+++ delphin/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from pages.views import view_home
 
@@ -8,3 +10,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', view_home, name='home'),
 ]
+
+# Check debug status
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
