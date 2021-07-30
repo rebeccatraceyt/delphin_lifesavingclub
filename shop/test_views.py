1cb1e3e565a0abb056e817b334086f8de55a9acf
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-30 14:52:28 +0000

    Test Shop views

---
+++ shop/test_views.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestAllCoursesView(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/shop/all_courses')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('all_courses'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('all_courses'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/all_courses.html')
+
+
+class TestAllApparelView(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/shop/all_apparel')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('all_apparel'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('all_apparel'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/all_apparel.html')
