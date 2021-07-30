68a39323ab80cb3086dc854028ed7a7523ff5fee
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-30 19:38:28 +0000

    Test: Search page views:

--- shop/test_views.py
+++ shop/test_views.py
@@ -18,6 +18,21 @@ class TestAllProductsView(TestCase):
         self.assertTemplateUsed(response, 'shop/shop_products.html')
 
 
+class TestSearchProductsView(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/shop/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('search'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('search'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/search_shop.html')
+
+
 class TestAllCoursesView(TestCase):
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/shop/all_courses')
