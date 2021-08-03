37c3ebb996b8dd6eccb225bf44b333572290f7a2
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-08-03 18:03:51 +0000

    Test: Shopping Bag view_bag view

---
+++ shopping_bag/test_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestShoppingBagView(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        """ Test URL is in correct location """
+        response = self.client.get('/shopping_bag/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        """ Test URL is accessible """
+        response = self.client.get(reverse('view_bag'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """ Test correct template """
+        response = self.client.get(reverse('view_bag'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shopping_bag/view_bag.html')
+
