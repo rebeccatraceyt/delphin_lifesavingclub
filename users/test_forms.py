d20f0a6f817a5456958755d7289c45799a1b2904
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-08-11 13:15:43 +0000

    Test: Users Forms

---
+++ users/test_forms.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .forms import ProfileForm
+
+
+class TestProfileRegistrationModel(TestCase):
+    """Test User Profile Form for registration """
+
+    def test_a_valid_registration(self):
+        """ Test User registration form """
+
+        form = ProfileForm({'email': 'test@gmail.com',
+                            'username': 'test',
+                            'password1': '!Testpassword2',
+                            'password2': '!Testpassword2', })
+        self.assertTrue(form.is_valid())
