3320085a2e5b30abdfa977749726b25c89b006e4
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-08-11 06:04:28 +0000

    Test: Product models

--- shop/test_models.py
+++ shop/test_models.py
@@ -4,8 +4,6 @@ from .models import (
     Size,
     Category,
     Product,
-    CourseTime,
-    ApparelSize
 )
 
 # ------ Product Categories ------
@@ -105,60 +103,3 @@ class TestProductModel(TestCase):
         self.assertEqual(product.image, "img.jpg")
         self.assertEqual(product.category_id, 1)
         self.assertEqual(product.time_id, 1)
-
-
-# ------ ManyToMany Through Tables ------
-
-
-class TestCourseTimeModel(TestCase):
-    """Test Course Time Model"""
-
-    def test_course_time_model(self):
-
-        # Create Test Time
-        time = Time.objects.create(time='Test Time')
-
-        # Create Test Course
-        product = Product(name='test course',
-                          description='test description',
-                          price='90.00',
-                          image='img.jpg',)
-
-        # Create Test Stock Count
-        course_time = CourseTime(stock_count=3)
-
-        # Save Test Course Time
-        course_time.time_id = time.id
-        course_time.product_id = product.id
-        course_time.save()
-
-        self.assertEqual(course_time.time_id, 1)
-        self.assertEqual(course_time.product.name, "test course")
-        self.assertEqual(course_time.stock_count, 3)
-
-
-class TestApparelSizeModel(TestCase):
-    """Test Apparel Size Model"""
-
-    def test_apparel_size_model(self):
-
-        # Create Test Size
-        size = Size.objects.create(size='Test Size')
-
-        # Create Test Course
-        product = Product.objects.create(name='test apparel',
-                                         description='test description',
-                                         price='90.00',
-                                         image='img.jpg',)
-
-        # Create Test Stock Count
-        apparel_size = ApparelSize(stock_count=3)
-
-        # Save Test Apparel Size
-        apparel_size.size_id = size.id
-        apparel_size.product_id = product.id
-        apparel_size.save()
-
-        self.assertEqual(apparel_size.size_id, 1)
-        self.assertEqual(apparel_size.product.name, "test apparel")
-        self.assertEqual(apparel_size.stock_count, 3)
