2cd633e8cd5394d6b880ad32a28031fb1f3fb614
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-08-11 12:51:14 +0000

    Test: Order views

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -1,26 +1,29 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from .models import Order
+from shop.models import Product
 
-class TestOrderReviewView(TestCase):
+
+class TestCheckoutView(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_view_url_exists_at_desired_location(self):
         """ Test URL is in correct location """
-        response = self.client.get('/checkout/')
+        response = self.client.get('/checkout/', follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
         """ Test URL is accessible """
-        response = self.client.get(reverse('order_review'))
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
 
     def test_view_uses_correct_template(self):
-        """ Test correct template """
-        response = self.client.get(reverse('order_review'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'checkout/order_review.html')
+        """ Test correct template redirects to all_products """
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/shop/')
 
 
 class TestOrderDetailsView(TestCase):
@@ -29,37 +32,56 @@ class TestOrderDetailsView(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
         """ Test URL is in correct location """
-        response = self.client.get('/checkout/')
+        response = self.client.get('/checkout/', follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
         """ Test URL is accessible """
         response = self.client.get(reverse('order_review'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_view_uses_correct_template(self):
-        """ Test correct template """
-        response = self.client.get(reverse('order_review'))
+        """ Test correct template redirects to all_products """
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/shop/')
+
+    def test_post_valid_order(self):
+        """ Test order post """
+        # login the user
+        self.client.login(username='username',
+                          password='password')
+
+        # create an item
+        item = Product.objects.create(name='test product',
+                                      price='22',
+                                      image='test.png',
+                                      description='test desc',)
+
+        # add item to cart
+        self.client.post("/shopping_bag/add/{0}".format(item.id),
+                         data={'quantity': '2'})
+
+        # get the stripe publisable key
+        stripe_id = 'tok_visa'
+
+        # send the order
+        response = self.client.post("/checkout/", {
+            'full_name': 'name',
+            'phone_number': '000',
+            'email': 'test@test.com',
+            'street_address1': 'address 1',
+            'street_address2': 'address 2',
+            'town_or_city': 'city',
+            'county': 'county',
+            'country': 'country',
+            'postcode': 'code13',
+            'credit_card_number': '4242424242424242',
+            'cvv': '123',
+            'expiry_month': '2',
+            'expiry_year': '2022',
+            'stripe_id': stripe_id},
+            follow=True)
+
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'checkout/order_review.html')
-
-
-class TestOrderDetailsView(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_view_url_exists_at_desired_location(self):
-        """ Test URL is in correct location """
-        response = self.client.get('/checkout/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        """ Test URL is accessible """
-        response = self.client.get(reverse('order_review'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        """ Test correct template """
-        response = self.client.get(reverse('order_review'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'checkout/order_review.html')
+        self.assertRedirects(response, '/shop/')
