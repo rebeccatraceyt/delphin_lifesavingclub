13a874c61db6a64baf80fc2198a618105a6ba47e
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2022-12-27 21:31:05 +0000

    Add Edit Product functionality

--- shop/views.py
+++ shop/views.py
@@ -188,3 +188,40 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """
+    Edit a product in the store
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that')
+        return redirect(reverse('home'))
+
+    # get the product
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        # instantiate form with specified instance being product chosen
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. \
+                Please ensure all fields are vaild')
+    else:
+        # instantiating the product form (using the product)
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'shop/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
