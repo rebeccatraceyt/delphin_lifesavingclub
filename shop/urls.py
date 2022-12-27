fbde97f0f53286575d7948da83f0ffd1f39a6bc1
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2022-12-27 22:42:34 +0000

    Fix Edit Product link on product detail page

--- shop/urls.py
+++ shop/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
         name='product_management'
         ),
     path('product_manager/add/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path(
-        'delete/<int:product_id>/',
+        'product_manager/edit/<int:product_id>/',
+        views.edit_product,
+        name='edit_product'),
+    path(
+        'product_manager/delete/<int:product_id>/',
         views.delete_product,
         name='delete_product'
         ),
